7d4785e8ec9cf982f509395fce1b7066ade1e271
Dan Mundy <drm.mundy@gmail.com>
2013-03-02 12:38:58 -0500

    Cleans up install.sh and makes uninstall.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,25 @@
 #! /bin/bash
 # from dpeticol's dotfiles, with mods
 
-ignore=( install.sh .git .gitmodules README.mdown backups )
+ignore=( install.sh uninstall.sh .git .gitmodules README.mdown backups )
 
 DIR=~/dotfiles
 
 cd $DIR
 mkdir backups/
-for filename in *
-do
+for filename in * do
     # check if the file should be ignored
     shouldIgnore=false
-    for ignorename in ${ignore[@]}
-    do
-        if [[ $filename == $ignorename ]]
-        then
+    for ignorename in ${ignore[@]} do
+        if [[ $filename == $ignorename ]] then
             shouldIgnore=true
         fi
     done
 
     # if you shouldn't ignore, and it's not already linked
-    if [ $shouldIgnore == false -a ! -L ~/.$filename ]
-    then
-
+    if [ $shouldIgnore == false -a ! -L ~/.$filename ] then
         # move old versions moved to backup dir
-        if [ -e ~/.$filename ]
-        then
+        if [ -e ~/.$filename ] then
             echo ${filename} moved to ${DIR}/backups/${filename}
             mv ~/.$filename $DIR/backups/
         fi
---
+++ uninstall.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+# from dpeticol's dotfiles, with mods
+
+ignore=( install.sh uninstall.sh .git .gitmodules README.mdown backups )
+
+DIR=~/dotfiles
+
+cd $DIR
+for filename in * do
+    # check if the file should be ignored
+    shouldIgnore=false
+    for ignorename in ${ignore[@]} do
+        if [[ $filename == $ignorename ]] then
+            shouldIgnore=true
+        fi
+    done
+
+    # if you shouldn't ignore and the file is a symlink
+    if [ $shouldIgnore == false -a -L ~/.$filename ] then
+        # Delete the symlink
+        echo deleted ${filename}
+        rm -rf ~/.$filename
+
+        # if the file exists as a backup, restore the backup
+        if [ -e $DIR/backups/.$filename ] then
+            echo ${filename} restored from ${DIR}/backups/${filename}
+            mv $DIR/backups/.$filename ~/
+        fi
+    fi
+done
+
+# update the submodules
+git submodule update --init
