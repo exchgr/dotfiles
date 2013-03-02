8eb33344c78bca290df8fa3702e439115b73d461
Dan Mundy <drm.mundy@gmail.com>
2013-03-02 12:46:36 -0500

    Conforms to bash's finicky syntax

--- install.sh
+++ install.sh
@@ -7,19 +7,24 @@ DIR=~/dotfiles
 
 cd $DIR
 mkdir backups/
-for filename in * do
+for filename in *
+do
     # check if the file should be ignored
     shouldIgnore=false
-    for ignorename in ${ignore[@]} do
-        if [[ $filename == $ignorename ]] then
+    for ignorename in ${ignore[@]}
+    do
+        if [[ $filename == $ignorename ]]
+        then
             shouldIgnore=true
         fi
     done
 
     # if you shouldn't ignore, and it's not already linked
-    if [ $shouldIgnore == false -a ! -L ~/.$filename ] then
+    if [ $shouldIgnore == false -a ! -L ~/.$filename ]
+    then
         # move old versions moved to backup dir
-        if [ -e ~/.$filename ] then
+        if [ -e ~/.$filename ]
+        then
             echo ${filename} moved to ${DIR}/backups/${filename}
             mv ~/.$filename $DIR/backups/
         fi
