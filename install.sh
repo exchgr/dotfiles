fb25e24c2a5c9c9fa3aef15913437bba1e7cc402
Dan Mundy <drm.mundy@gmail.com>
2013-08-27 16:25:25 -0400

    Only mkdir backups, clone vundle, and compile YCM if each doesn't exist

--- install.sh
+++ install.sh
@@ -7,44 +7,52 @@ DIR=~/dotfiles
 OS=$(uname -s)
 
 cd $DIR
-mkdir backups/
+
+if [ ! -e ${DIR}/backups ]; then
+  mkdir backups
+fi
+
 for filename in *
 do
-    # check if the file should be ignored
-    shouldIgnore=false
-    for ignorename in ${ignore[@]}
-    do
-        if [[ $filename == $ignorename ]]
-        then
-            shouldIgnore=true
-        fi
-    done
-
-    # if you shouldn't ignore, and it's not already linked
-    if [ $shouldIgnore == false -a ! -L ~/.$filename ]
+  # check if the file should be ignored
+  shouldIgnore=false
+  for ignorename in ${ignore[@]}
+  do
+    if [[ $filename == $ignorename ]]
     then
-        # move old versions moved to backup dir
-        if [ -e ~/.$filename ]
-        then
-            echo ${filename} moved to ${DIR}/backups/${filename}
-            mv ~/.$filename $DIR/backups/
-        fi
-
-        # create the link
-        echo new link ~/.${filename} to ${DIR}/backups/${filename}
-        ln -s $DIR/$filename ~/.$filename
+      shouldIgnore=true
     fi
+  done
+
+  # if you shouldn't ignore, and it's not already linked
+  if [ $shouldIgnore == false -a ! -L ~/.$filename ]
+  then
+    # move old versions moved to backup dir
+    if [ -e ~/.$filename ]
+    then
+      echo ${filename} moved to ${DIR}/backups/${filename}
+      mv ~/.$filename $DIR/backups/
+    fi
+
+    # create the link
+    echo new link ~/.${filename} to ${DIR}/backups/${filename}
+    ln -s $DIR/$filename ~/.$filename
+  fi
 done
 
 # Install Vundle and packages
-git clone https://github.com/gmarik/vundle ${DIR}/vim/bundle/vundle
-vim +BundleInstall +qa
+if [ ! -e ${DIR}/vim/bundle/vundle ]; then
+  git clone https://github.com/gmarik/vundle ${DIR}/vim/bundle/vundle
+  vim +BundleInstall +qa
+fi
 
 # Compile YouCompleteMe
 if [ "$OS" = "Linux" ]; then
   sudo apt-get install build-essential cmake python-dev
 fi
 
-cd ~/.vim/bundle/YouCompleteMe
-./install.sh --clang-completer
-cd -
+if [ ! -e ${DIR}/vim/bundle/YouCompleteMe/python/ycm_core.so ]; then
+  cd ${DIR}/vim/bundle/YouCompleteMe
+  ./install.sh --clang-completer
+  cd -
+fi
